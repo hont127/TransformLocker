bdf371a443127bb4ec79a915f8e39a29521eb344
hont127@163.com <hont127@163.com>
2018-08-22 09:09:28 +0800

    Signed-off-by: hont127@163.com <hont127@163.com>

---
+++ TransformLocker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformLocker : MonoBehaviour
+{
+    bool mIsInitialization;
+
+
+    void OnValidate()
+    {
+#if UNITY_EDITOR
+        if (!mIsInitialization)
+        {
+            UnityEditor.EditorApplication.update -= SceneUpdateCallback;
+            UnityEditor.EditorApplication.update += SceneUpdateCallback;
+            mIsInitialization = true;
+        }
+#endif
+    }
+
+    void Reset()
+    {
+#if UNITY_EDITOR
+        if (!mIsInitialization)
+        {
+            UnityEditor.EditorApplication.update -= SceneUpdateCallback;
+            UnityEditor.EditorApplication.update += SceneUpdateCallback;
+            mIsInitialization = true;
+        }
+#endif
+    }
+
+#if UNITY_EDITOR
+    void SceneUpdateCallback()
+    {
+        try
+        {
+            transform.GetHashCode();
+        }
+        catch
+        {
+            UnityEditor.EditorApplication.update -= SceneUpdateCallback;
+            return;
+        }
+
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+    }
+#endif
+}
